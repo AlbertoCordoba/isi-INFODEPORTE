23862697fa3a0b4416295f2cd6e052bd9a93cbb4
Alberto Córdoba Ruiz <alberto.cordoba1@alu.uclm.es>
2025-03-29 19:38:59 +0100

    Añadiendo proyecto a la carpeta Sprint3 en la rama main

--- BotTelegram/tests/conftest.py
+++
@@ -1,9 +0,0 @@
-import pytest
-from database import Database
-
-@pytest.fixture
-def test_db():
-    """Fixture para la base de datos de pruebas"""
-    db = Database(":memory:")  # Usa una base de datos en memoria
-    yield db
-    db.close()
